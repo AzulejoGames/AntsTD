af6abe7cc00fbd56378edff373481802d5fd3676
eduardo <osvino.sabino@gmail.com>
2026-09-09 20:24:26 -0300

    fiz o sistema de compra das formigas

--- Assets/Scripts/Novos scripsts/SistemaDeConstrucao.cs
+++ Assets/Scripts/Novos scripsts/SistemaDeConstrucao.cs
@@ -6,17 +6,23 @@ public class SistemaDeConstrucao : MonoBehaviour
 
     private GameObject torreSelecionada;
     private InputCabeca inputCabeca;
+    private BaseHealth baseHealth;
 
     private void Awake()
     {
         inputCabeca = FindFirstObjectByType<InputCabeca>();
+        baseHealth = FindFirstObjectByType<BaseHealth>();
+
     }
+  
 
     public void SelecionarTorre(GameObject torre)
     {
         torreSelecionada = torre;
 
         Debug.Log("Torre selecionada: " + torre.name);
+
+      
     }
 
     private void OnEnable()
@@ -32,16 +38,41 @@ public class SistemaDeConstrucao : MonoBehaviour
     private void IniciarConstrucao()
     {
         if (torreSelecionada == null)
-            return;
+        return;
 
-        Vector3 posicao = cam.ScreenToWorldPoint(inputCabeca.PosicaoInput);
+    AtaqueBase ataqueBase = torreSelecionada.GetComponent<AtaqueBase>();
 
-        posicao.z = 0;
-
-        Debug.Log("Torre colocada em: " + posicao);
-
-        Instantiate(torreSelecionada, posicao, Quaternion.identity);
-
-        torreSelecionada = null;
+    if (ataqueBase == null)
+    {
+        Debug.LogError("A torre não possui AtaqueBase.");
+        return;
     }
-}   
+
+    int custo = ataqueBase.Custo;
+
+    if (baseHealth.pontos < custo)
+    {
+        Debug.Log("Pontos insuficientes!");
+        Debug.Log("Pontos atuais: " + baseHealth.pontos);
+        Debug.Log("Custo da torre: " + custo);
+        return;
+    }
+
+    Vector3 posicao = cam.ScreenToWorldPoint(inputCabeca.PosicaoInput);
+
+    posicao.z = 0f;
+
+    Instantiate(
+        torreSelecionada,
+        posicao,
+        Quaternion.identity
+    );
+
+    baseHealth.pontos -= custo;
+
+    Debug.Log("Torre construída!");
+    Debug.Log("Pontos restantes: " + baseHealth.pontos);
+
+    torreSelecionada = null;
+    }   
+}
